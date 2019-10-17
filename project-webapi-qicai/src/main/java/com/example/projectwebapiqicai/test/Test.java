ceaf9eb678054ac59163bb85b364f3cace339b27
lijunhui <lijh@watone.com.cn>
2019-10-17 17:25:46 +0800

    测试

---
+++ project-webapi-qicai/src/main/java/com/example/projectwebapiqicai/test/Test.java
@@ -0,0 +1,15 @@
+package com.example.projectwebapiqicai.test;
+
+/**
+ * @program: project-manage
+ * @description
+ * @author: lijunhui
+ * @create: 2019-09-27 09:47
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+
+    }
+}
